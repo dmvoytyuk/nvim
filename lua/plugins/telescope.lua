7a82925be151ab4c07627577b985479dc4b8c207
dmvoytyuk <dmvoytyuk@gmail.com>
2024-03-10 12:56:47 +0100

    lualine + winbar

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,7 +2,7 @@ return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.5",
 	dependencies = {
-		"nvim-lua/plenary.nvim"
+		"nvim-lua/plenary.nvim",
 	},
 	config = function()
 		local builtin = require('telescope.builtin')
