7e2b9559f78df0d585dc4a1bae7b4bcf856e3333
dmvoytyuk <dmvoytyuk@gmail.com>
2024-03-10 12:19:52 +0100

    onedark theme + modular plugins

--- init.lua
+++ init.lua
@@ -19,42 +19,4 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-local plugins = {
-	--{ 'sainnhe/sonokai', name = 'sonokai', priority = 1000 },
-	{
-		"sainnhe/edge",
-		lazy = false,
-		priority = 1000,
-	},
-	{
-		"nvim-telescope/telescope.nvim", tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" }
-    },
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
-}
-local opts = {}
-
-require("lazy").setup(plugins, opts)
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-local configs = require('nvim-treesitter.configs')
-configs.setup({
-	ensure_installed = {"lua", "javascript", "css", "html"},
-	highlight = { enable = true },
-	indent = { enable = true },
-})
-
-vim.g.edge_better_performance = 1
-vim.g.edge_enable_italic = 1
-vim.g.edge_transparent_background = 1
-vim.cmd.colorscheme "edge"
-
---vim.g.sonokai_style = "andromeda"
---vim.g.sonokai_better_performance = 1
---vim.g.sonokai_enable_italic = 1
---vim.g.sonokai_transparent_background = 1
---vim.cmd.colorscheme "sonokai"
-
+require("lazy").setup("plugins")
---
+++ lua/plugins/onedark.lua
@@ -0,0 +1,16 @@
+return {
+	"navarasu/onedark.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("onedark").setup {
+			style = "deep",
+			transparent = true,
+			code_style = {
+				keywords = "italic",
+				functions = "bold",
+			}
+		}
+		vim.cmd.colorscheme "onedark"
+	end
+}
