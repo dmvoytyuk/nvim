2d43f3c851c2d2e3857fdb3d728e2acb8aac28d5
dmvoytyuk <dmvoytyuk@gmail.com>
2024-03-11 23:26:08 +0100

    colorscheme

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,31 @@
+return
+-- {
+--	"navarasu/onedark.nvim",
+--	lazy = false,
+--	priority = 1000,
+--	config = function()
+--		require("onedark").setup {
+--			style = "darker",
+--			transparent = true,
+--			code_style = {
+--				keywords = "italic",
+--				functions = "bold",
+--			}
+--		}
+--		vim.cmd.colorscheme "onedark"
+--	end
+--}
+{
+	"folke/tokyonight.nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("tokyonight").setup({
+			style = "night",
+			styles = {
+				functions = { bold = true },
+			}
+		})
+		vim.cmd.colorscheme "tokyonight"
+	end
+}
