211e7bf9a0dd30e4398f7674977410e40250fcb6
dmvoytyuk <dmvoytyuk@gmail.com>
2024-03-14 01:25:50 +0100

    nvim-cmp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,30 +1,39 @@
 return {
 	{
-    	"williamboman/mason.nvim",
+		"williamboman/mason.nvim",
 		config = function()
 			require("mason").setup()
-		end
+		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "cssls", "html", "tsserver" }
+				ensure_installed = { "lua_ls", "cssls", "html", "tsserver" },
 			})
-		end
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
-			lspconfig.tsserver.setup({})
-			lspconfig.cssls.setup({})
-			lspconfig.html.setup({})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.tsserver.setup({
+				capabilities = capabilities
+			})
+			lspconfig.cssls.setup({
+				capabilities = capabilities
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities
+			})
 
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end
-	}
+		end,
+	},
 }
