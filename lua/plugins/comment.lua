40a073263391568b49ecf949f33d51e68c3e1864
dmvoytyuk <dmvoytyuk@gmail.com>
2024-03-17 00:15:37 +0100

    comments plugin

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,7 +1,14 @@
 return {
-	'numToStr/Comment.nvim',
-	lazy = false,
-	config = function ()
-		require('Comment').setup()
-	end
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+	},
+	{
+		"numToStr/Comment.nvim",
+		lazy = false,
+		config = function()
+			require("Comment").setup({
+				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			})
+		end,
+	},
 }
