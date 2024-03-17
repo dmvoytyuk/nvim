8fe9f8872913c4fdac39a9043146421e19130ab4
dmvoytyuk <dmvoytyuk@gmail.com>
2024-03-17 12:01:48 +0100

    autoformat on save

--- lua/plugins/complettions.lua
+++ lua/plugins/complettions.lua
@@ -22,6 +22,7 @@ return {
 			local cmp = require("cmp")
 			local luasnip = require("luasnip")
 			require("luasnip.loaders.from_vscode").lazy_load()
+			luasnip.filetype_extend("javascriptreact", { " html " })
 
 			cmp.setup({
 				snippet = {
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -20,6 +20,12 @@ return {
 			matchup = {
 				enable = true,
 			},
+			-- autotag = {
+			-- 	enable = true,
+			-- 	enable_rename = true,
+			-- 	enable_close = true,
+			-- 	enable_close_on_slash = true,
+			-- },
 		})
 	end,
 }
